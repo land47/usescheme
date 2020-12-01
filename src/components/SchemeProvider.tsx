ee53963074068fd8203549d2e06ad1ba5c4298c8
Anton <jjoska@yandex.ru>
2020-12-01 17:32:55 +0300

    feat(components/SchemeProvider): now `initial` prop is optional

--- src/components/SchemeProvider.tsx
+++ src/components/SchemeProvider.tsx
@@ -1,37 +1,40 @@
-import { FC, useEffect, useState } from "react"
+import { FC, useEffect, useState } from "react";
 import { SchemeContext } from "../contexts";
 import { Scheme } from "../types";
-import { appearanceByScheme, stringToScheme } from "../utils"
+import { appearanceByScheme, stringToScheme } from "../utils";
 import bridge, { VKBridgeSubscribeHandler } from "@vkontakte/vk-bridge";
-import { ConfigProvider } from "@vkontakte/vkui"
+import { ConfigProvider } from "@vkontakte/vkui";
 
 export type SchemeProviderProps = {
-    initial: Scheme;
-}
+  initial?: Scheme;
+};
 
-const SchemeProvider: FC<SchemeProviderProps> = ({ initial, children }) => {
-    const [scheme, setScheme] = useState<Scheme>(initial)
+const SchemeProvider: FC<SchemeProviderProps> = ({
+  initial = Scheme.LIGHT,
+  children,
+}) => {
+  const [scheme, setScheme] = useState<Scheme>(initial);
 
-    useEffect(() => {
-        const themeCatcher: VKBridgeSubscribeHandler = ({ detail }) => {
-            if (detail.type !== "VKWebAppUpdateConfig") {
-                return;
-            }
+  useEffect(() => {
+    const themeCatcher: VKBridgeSubscribeHandler = ({ detail }) => {
+      if (detail.type !== "VKWebAppUpdateConfig") {
+        return;
+      }
 
-            setScheme(stringToScheme(detail.data.scheme));
-        }
+      setScheme(stringToScheme(detail.data.scheme));
+    };
 
-        bridge.subscribe(themeCatcher);
-        return () => bridge.unsubscribe(themeCatcher);
-    }, [])
+    bridge.subscribe(themeCatcher);
+    return () => bridge.unsubscribe(themeCatcher);
+  }, []);
 
-    return (
-        <SchemeContext.Provider value={{ scheme, setScheme }}>
-            <ConfigProvider scheme={scheme} appearance={appearanceByScheme(scheme)}>
-                {children}
-            </ConfigProvider>
-        </SchemeContext.Provider>
-    );
-}
+  return (
+    <SchemeContext.Provider value={{ scheme, setScheme }}>
+      <ConfigProvider scheme={scheme} appearance={appearanceByScheme(scheme)}>
+        {children}
+      </ConfigProvider>
+    </SchemeContext.Provider>
+  );
+};
 
 export default SchemeProvider;
