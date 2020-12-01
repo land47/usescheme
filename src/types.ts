6b48c94fdc21ac762d53e5a4487646572a874390
Anton <jjoska@yandex.ru>
2020-12-01 16:37:36 +0300

    feat(types): define a type for a theme control object

--- src/types.ts
+++ src/types.ts
@@ -2,3 +2,9 @@ export enum Scheme {
   DARK = "space_gray",
   LIGHT = "bright_light",
 }
+
+export type SchemeControls = {
+  setScheme(scheme: Scheme): void;
+  toggleScheme(): void;
+  scheme: Scheme;
+};
