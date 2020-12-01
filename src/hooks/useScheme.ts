a947969592544695163075246e72caccc11e1edb
Anton <jjoska@yandex.ru>
2020-12-01 17:00:18 +0300

    refactor(hooks/useScheme): refactor to use `SchemeContext`

--- src/hooks/useScheme.ts
+++ src/hooks/useScheme.ts
@@ -1,11 +1,16 @@
-export default function useScheme() {
-  return {
-    setScheme() {
-      //
-    },
+import { useContext } from "react";
+import { SchemeContext } from "../contexts";
+import { SchemeControls } from "../types";
+import { alternativeScheme } from "../utils";
 
+export default function useScheme(): SchemeControls {
+  const { scheme, setScheme } = useContext(SchemeContext);
+
+  return {
+    setScheme,
     toggleScheme() {
-      //
+      setScheme(alternativeScheme(scheme));
     },
+    scheme,
   };
 }
