de3179d7969aab78642a8f52b914186ce5ebfea1
Anton <jjoska@yandex.ru>
2020-12-01 16:58:17 +0300

    feat(utils): re-export `appearanceByScheme()`

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,3 @@
 export { default as alternativeScheme } from "./alternativeScheme";
 export { default as stringToScheme } from "./stringToScheme";
+export { default as appearanceByScheme } from "./appearanceByScheme";
