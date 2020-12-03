61ea5d036e14df1bb9acf751b5488ff215f62847
Anton <jjoska@yandex.ru>
2020-12-03 21:50:26 +0300

    feat(utils): re-export `withStorage`

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,3 +3,4 @@ export { default as stringToScheme } from "./stringToScheme";
 export { default as appearanceByScheme } from "./appearanceByScheme";
 export { default as schemeFromStorage } from "./schemeFromStorage";
 export { default as delay } from "./delay";
+export { default as withStorage } from "./withStorage";
