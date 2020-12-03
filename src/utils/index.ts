74b4b847f97719b7582f641b42a908cf883e9356
Anton <jjoska@yandex.ru>
2020-12-03 21:31:20 +0300

    refactor(utils): re-export `delay()`

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,5 @@
 export { default as alternativeScheme } from "./alternativeScheme";
 export { default as stringToScheme } from "./stringToScheme";
 export { default as appearanceByScheme } from "./appearanceByScheme";
+export { default as schemeFromStorage } from "./schemeFromStorage";
+export { default as delay } from "./delay";
