2dd06e1362d92030a148b827ce2a589975ee9fad
Anton <jjoska@yandex.ru>
2020-12-03 22:35:45 +0300

    feat: re-export `SchemeControls`

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { SchemeProvider } from "./components";
 export { useScheme } from "./hooks";
-export type { Scheme } from "./types";
+export type { Scheme, SchemeControls } from "./types";
 export { withScheme } from "./hoc";
