ebafe1358ae41cc99a0fba12f7a19b71839a6c5e
Anton <jjoska@yandex.ru>
2020-12-03 22:37:31 +0300

    refactor(hoc/withScheme): added field `displayName` and moved `controls` outside `render()`

--- src/hoc/withScheme.tsx
+++ src/hoc/withScheme.tsx
@@ -1,26 +1,24 @@
-import { Component, ComponentType } from "react";
+import { Component, ComponentType, } from "react";
 import { SchemeContext } from "../contexts";
-import { Scheme, SchemeControls } from "../types";
+import { SchemeControls } from "../types";
 import { alternativeScheme } from "../utils";
 
-export default function withScheme(Element: ComponentType<SchemeControls>) {
-  return class extends Component {
+export default function withScheme<P>(Child: ComponentType<P>) {
+  return class extends Component<SchemeControls & P, {}> {
     static contextType = SchemeContext;
+    static displayName = `withScheme(${Child.displayName || Child.name})`;
 
-    render() {
-      const { scheme, setScheme } = this.context;
+    controls: SchemeControls = {
+      scheme: this.context.scheme,
+      setScheme: this.context.setScheme,
+      toggleScheme: () => {
+        this.context.setScheme(alternativeScheme(this.context.scheme))
+      }
+    }
 
-      const controls: SchemeControls = {
-        scheme,
-        setScheme: (scheme: Scheme) => {
-          setScheme(scheme);
-        },
-        toggleScheme: () => {
-          setScheme(alternativeScheme(scheme));
-        },
-      };
-
-      return <Element {...controls} />;
+    render = () => {
+      return <Child {...this.props} {...this.controls} />;
     }
   };
+
 }
