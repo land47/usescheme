632d59227e99a40d539c8f455f00a47cdd5ae15d
Anton <jjoska@yandex.ru>
2020-12-05 20:21:34 +0300

    refactor(utils/schemeFromStorage): remove zero latency

--- src/utils/schemeFromStorage.ts
+++ src/utils/schemeFromStorage.ts
@@ -5,9 +5,5 @@ import { delay } from "../utils";
  * Возвращает сохранённую в `VKStorage` тему.
  */
 export default async function schemeFromStorage() {
-  /**
-   * Фикс бага. Почему так – не ясно. Почему фиксит – тоже не ясно.
-   */
-  await delay(0);
   return storage.get("scheme").then(({ keys }) => keys[0].value);
 }
