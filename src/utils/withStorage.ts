8cb487cea0fb2b4140f41670374472fb67e4dc23
Anton <jjoska@yandex.ru>
2020-12-05 13:41:43 +0300

    refactor(utils/withStorage): use throttling for set to storage

--- src/utils/withStorage.ts
+++ src/utils/withStorage.ts
@@ -1,13 +1,22 @@
+import { stringToScheme, throttle } from "../utils";
+import { Scheme } from "../types";
 import { storage } from "@_themis/vkstorage";
 
-export default function withStorage<T, A extends T[], R>(
-  func: (...args: A) => R,
-) {
-  return (...args: A) => {
-    func(...args);
+const withStorage = <T, A extends [T], R>(func: (...args: A) => R) => {
+  function setSchemeToStorage(scheme: Scheme) {
+    storage.set("scheme", scheme);
+  }
 
-    if (typeof args[0] === "string") {
-      storage.set("scheme", args[0] + "");
+  const throttled = throttle(setSchemeToStorage, 900);
+
+  return (...args: A) => {
+    const [scheme] = args;
+
+    if (typeof scheme === "string") {
+      throttled(stringToScheme(scheme));
+      func(...args);
     }
   };
-}
+};
+
+export default withStorage;
