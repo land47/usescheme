dfd62e3ee9568568f9bc5dd697b3f4c95a37a3bc
Anton <jjoska@yandex.ru>
2020-12-07 15:36:58 +0300

    refactor: update imports path for `types.ts`

--- src/contexts/SchemeContext.ts
+++ src/contexts/SchemeContext.ts
@@ -1,5 +1,5 @@
 import { createContext } from "react";
-import { Scheme } from "../types";
+import { Scheme } from "../shared/types";
 
 export default createContext(
   { scheme: Scheme.LIGHT, setScheme(scheme: Scheme) {/** Initial value */} },
