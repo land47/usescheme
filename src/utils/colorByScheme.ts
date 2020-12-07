18e9361215ae8aac513e3ba52eaa0833b8f17238
Anton <jjoska@yandex.ru>
2020-12-07 16:15:42 +0300

    refactor(utils/updateNativeBars): remove excessary helper

--- src/utils/colorByScheme.ts
+++
@@ -1,5 +0,0 @@
-import { Scheme } from "../shared/types";
-
-export default function colorByScheme(scheme: Scheme) {
-  return scheme === Scheme.DARK ? "#19191a" : "#fff";
-}
--- src/utils/updateNativeBars.ts
+++ src/utils/updateNativeBars.ts
@@ -1,6 +1,5 @@
 import bridge from "@vkontakte/vk-bridge";
 import { Scheme } from "../shared/types";
-import { colorByScheme } from "../utils";
 import { IS_WEBVIEW } from "../shared/constants";
 
 export default function updateNativeBars(currentScheme: Scheme) {
@@ -8,10 +7,8 @@ export default function updateNativeBars(currentScheme: Scheme) {
     return;
   }
 
-  const color = colorByScheme(currentScheme);
-
   return bridge.send("VKWebAppSetViewSettings", {
     status_bar_style: currentScheme === Scheme.LIGHT ? "dark" : "light",
-    action_bar_color: color,
+    action_bar_color: currentScheme === Scheme.LIGHT ? "#fff" : "#19191a",
   });
 }
