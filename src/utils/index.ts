b36d7e379d3e33daee24c6927a167bd9710cdae3
Anton <jjoska@yandex.ru>
2020-12-07 16:16:23 +0300

    refactor(utils): remove re-export `colorByScheme()`

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,6 +3,5 @@ export { default as stringToScheme } from "./stringToScheme";
 export { default as appearanceByScheme } from "./appearanceByScheme";
 export { default as schemeFromStorage } from "./schemeFromStorage";
 export { default as withStorage } from "./withStorage";
-export { default as colorByScheme } from "./colorByScheme";
 export { default as throttle } from "./throttle";
 export { default as updateNativeBars } from "./updateNativeBars";
